26d35c9e04ca2768a3ec2424b43133262f48341b
a483210 <a483210@163.com>
2019-10-16 11:47:04 +0800

    添加SpringBootApplication路径获取

--- library/src/main/java/com/mnazareno/retrofit/springboot/RetrofitClientsRegistrar.java
+++ library/src/main/java/com/mnazareno/retrofit/springboot/RetrofitClientsRegistrar.java
@@ -1,8 +1,11 @@
 package com.mnazareno.retrofit.springboot;
 
 import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
 import org.springframework.core.type.AnnotationMetadata;
@@ -31,7 +34,7 @@ public class RetrofitClientsRegistrar implements ImportBeanDefinitionRegistrar {
         };
         scanner.addIncludeFilter(new AnnotationTypeFilter(RetrofitClient.class));
 
-        Set<String> basePackages = getBasePackages(metadata);
+        Set<String> basePackages = getBasePackages(metadata, registry);
 
         basePackages.stream()
                 .map(scanner::findCandidateComponents)
@@ -73,7 +76,8 @@ public class RetrofitClientsRegistrar implements ImportBeanDefinitionRegistrar {
                 builder.getBeanDefinition());
     }
 
-    private Set<String> getBasePackages(AnnotationMetadata importingClassMetadata) {
+    private Set<String> getBasePackages(AnnotationMetadata importingClassMetadata,
+                                        BeanDefinitionRegistry registry) {
         Map<String, Object> attributes = importingClassMetadata
                 .getAnnotationAttributes(EnableRetrofitClients.class.getCanonicalName());
 
@@ -97,9 +101,31 @@ public class RetrofitClientsRegistrar implements ImportBeanDefinitionRegistrar {
         }
 
         if (basePackages.isEmpty()) {
-            basePackages.add(
-                    ClassUtils.getPackageName(importingClassMetadata.getClassName()));
+            String applicationPackage = getApplicationPackage(registry);
+            if (!StringUtils.isEmpty(applicationPackage)) {
+                basePackages.add(applicationPackage);
+            } else {
+                basePackages.add(ClassUtils.getPackageName(importingClassMetadata.getClassName()));
+            }
         }
+
         return basePackages;
+    }
+
+    private String getApplicationPackage(BeanDefinitionRegistry registry) {
+        for (String beanName : registry.getBeanDefinitionNames()) {
+            BeanDefinition obj = registry.getBeanDefinition(beanName);
+            if (!(obj instanceof AbstractBeanDefinition)) {
+                continue;
+            }
+            AbstractBeanDefinition beanDef = ((AbstractBeanDefinition) obj);
+            Class<?> cls = beanDef.getBeanClass();
+            SpringBootApplication springBootApplication = cls.getAnnotation(SpringBootApplication.class);
+            if (springBootApplication != null) {
+                return cls.getPackage().getName();
+            }
+        }
+
+        return null;
     }
 }
